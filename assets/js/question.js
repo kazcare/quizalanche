bd6b6499007a65c92cbec525374b7fee5fe20301
kazcare <kazcare@hotmail.com>
2022-01-24 22:55:16 +0000

    Add Questions in question.js

---
+++ assets/js/question.js
@@ -0,0 +1,122 @@
+const questions = [
+    {
+    "question": "What does DNA stand for?",
+    "option1": "Ribonucleic Acid",
+    "option2": "Deoxyribonucleic Acid",
+    "option3": "Deoxy Nitric Acid",
+    "option4": "None of the rest",
+    "answer": "2"
+},
+{
+    "question": "At what temperature are Celsius and Fahrenheit equal?",
+    "option1": "-40",
+    "option2": "32",
+    "option3": "212",
+    "option4": "100",
+    "answer": "1"
+},
+{
+    "question": "Roughly how long does it take for the sun's light to reach Earth?",
+    "option1": "8 hours",
+    "option2": "4 hours",
+    "option3": "8 minutes",
+    "option4": "20 minutes",
+    "answer": "3"
+},
+{
+    "question": "What modern-day country was Marie Curie born in?",
+    "option1": "Poland",
+    "option2": "Greece",
+    "option3": "France",
+    "option4": "Germany",
+    "answer": "1"
+},
+{
+    "question": "How many bones are in the human body?",
+    "option1": "196",
+    "option2": "189",
+    "option3": "212",
+    "option4": "206",
+    "answer": "4"
+},
+{
+    "question": "The concept of gravity was discovered by which famous physicist?",
+    "option1": "Albert Einstein",
+    "option2": "Galileo Galilei",
+    "option3": "Sir Isaac Newton",
+    "option4": "Marie Curie",
+    "answer": "3"
+},
+{
+    "question": "What is the hardest natural substance on Earth?",
+    "option1": "Aluminium",
+    "option2": "Diamond",
+    "option3": "Copper",
+    "option4": "Iron",
+    "answer": "2"
+},
+{
+    "question": "What is the biggest planet in our solar system?",
+    "option1": "Uranus",
+    "option2": "Mars",
+    "option3": "Saturn",
+    "option4": "Jupiter",
+    "answer": "4"
+},
+{
+    "question": "How many teeth does an adult human have?",
+    "option1": "32",
+    "option2": "28",
+    "option3": "30",
+    "option4": "24",
+    "answer": "1"
+},
+{
+    "question": "What was the name of the first man-made satellite launched by the Soviet Union in 1957?",
+    "option1": "Mir",
+    "option2": "Salyut",
+    "option3": "Sputnik 1",
+    "option4": "Poisk",
+    "answer": "3"
+},
+{
+    "question": "Which is the main gas that makes up the Earth's atmosphere?",
+    "option1": "Nitrogen",
+    "option2": "Oxygen",
+    "option3": "Argon",
+    "option4": "Carbon Dioxide",
+    "answer": "1"
+},
+{
+    "question": "Which famous British physicist wrote A Brief History of Time?",
+    "option1": "Michael Faraday",
+    "option2": "Charles darwin",
+    "option3": "Alexander Fleming",
+    "option4": "Stephen Hawking",
+    "answer": "4"
+},
+{
+    "question": "What name is given for the number of protons found in the nucleus of an atom?",
+    "option1": "Atomic Mass",
+    "option2": "Atomic Number",
+    "option3": "Atomic Weight",
+    "option4": "Atomic Mass Unit",
+    "answer": "2"
+},
+{
+    "question": "How many vertebrae does the average human possess?",
+    "option1": "33",
+    "option2": "28",
+    "option3": "25",
+    "option4": "39",
+    "answer": "1"
+},
+{
+    "question": "What is a material that will not carry an electrical charge called?",
+    "option1": "Conductor",
+    "option2": "Semi-Conductor",
+    "option3": "Insulator",
+    "option4": "Convector",
+    "answer": "3"
+}
+];
